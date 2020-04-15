944ef7c537755eab1be63af8306150cee59c8bb4
MehdiChaouche <mehdi.chaouche@le-campus-numerique.fr>
2020-04-15 09:37:39 +0200

    Serveur fonctionnel, fichiers statiques servis

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ let express = require('express');
 let app = express();
 
 console.log('test');
+app.use(express.static('frontend'));
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
 
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
